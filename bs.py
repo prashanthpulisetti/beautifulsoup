86032a7f64dd3ecc2a39215852182efa83245187
sai prashanth pulisetti <40313110+prashanthpulisetti@users.noreply.github.com>
2019-01-28 10:43:18 +0530

    Update bs.py

--- bs.py
+++ bs.py
@@ -2,7 +2,7 @@ import bs4
 from urllib.request import urlopen as uReq
 import bs4
 
-my_url ='http://www.nsvdc.com'
+my_url ='<give your url which you want to dump>'
 uClient = uReq(my_url)
 page_html = uClient.text
 page_soup =soup(page_html,"html.paser")
